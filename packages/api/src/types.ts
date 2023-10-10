ed4a20bebc4ff262312a9de92ed1ac35581fc70e
baran.wang <baran.wang@bytedance.com>
2023-10-10 11:06:54 +0800

    feat: add device types

--- packages/api/src/types.ts
+++ packages/api/src/types.ts
@@ -27,3 +27,62 @@ export interface GetFamilyListResponse {
   createfamilies: FamilyInfo[];
   joinfamilies: FamilyInfo[];
 }
+
+export interface DeviceInfo {
+  baseInfo: DeviceBaseInfo;
+  extendedInfo: DeviceExtendedInfo;
+}
+
+export interface DeviceBaseInfo {
+  deviceId: string;
+  deviceName: string;
+  devName: string;
+  bindTime: string;
+  deviceType: string;
+  familyId: string;
+  ownerId: string;
+  permission: {
+    auth: {
+      control: boolean;
+      set: boolean;
+      view: boolean;
+    };
+    authType: string;
+  };
+  wifiType: string | null;
+  isOnline: boolean;
+  ownerInfo: any;
+  subDeviceIds: string | null;
+  parentsDeviceId: string | null;
+  deviceRole: null;
+  deviceRoleType: string | null;
+  deviceNetType: 'device' | 'nonNetDevice';
+  deviceGroupId: string | null;
+  deviceGroupType: string | null;
+}
+
+export interface DeviceExtendedInfo {
+  brand: string;
+  apptypeCode: string;
+  apptypeName: string;
+  apptypeIcon: string;
+  barcode: string;
+  room: string;
+  roomId: string;
+  devFloorId: string;
+  devFloorOrderId: string;
+  devFloorName: string;
+  model: string;
+  prodNo: string;
+  bindType: string | null;
+  categoryGrouping: string;
+  imageAddr1: string;
+  imageAddr2: string;
+  accessType: string | null;
+  configType: string;
+  comunicationMode: string | null;
+  detailsUrl: string | null;
+  noKeepAlive: number;
+  twoGroupingName: string;
+  appletProxAuth: number;
+}
